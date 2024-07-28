f6f87a24fbd3f5fcd85ad5c278a94964980b449c
deuvede24 <cdaniela.vd@gmail.com>
2024-07-28 22:44:26 +0200

    reset project ok

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { FullcalendarComponent } from './components/fullcalendar/fullcalendar.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { UsersComponent } from './components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'fullcalendar', component: FullcalendarComponent },
+  { path: 'charts', component: ChartsComponent },
+  { path: 'users', component: UsersComponent }
+];
